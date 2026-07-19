6e6b1071b99d1450fe534d34ccdbc4613276322d
_theNorthSea_ <3349789097@qq.com>
2026-07-19 16:40:15 +0800

    feat(后端): 搭建 Spring Boot 微服务骨架

---
+++ services/notification-service/src/test/java/com/petadoption/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.petadoption.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
